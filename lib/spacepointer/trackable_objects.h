ca8a7dba22d68934f33c480c285fa13a8404139e
Anthony Bryant <anthony@bryants.eu>
2022-06-19 22:07:08 +0100

    Add an initial list of trackable objects, to be expanded

---
+++ lib/spacepointer/trackable_objects.h
@@ -0,0 +1,11 @@
+#ifndef SPACEPOINTER_LIB_SPACEPOINTER_TRACKABLE_OBJECTS_H_
+#define SPACEPOINTER_LIB_SPACEPOINTER_TRACKABLE_OBJECTS_H_
+
+#include "equatorial_location.h"
+#include "location.h"
+
+const Location LONDON = Location(51.5008044929728, -0.12434070813135319, 0);
+const EquatorialLocation POLARIS = EquatorialLocation(37.9500, 89.2642);
+const EquatorialLocation URSA_MAJOR = EquatorialLocation(160.05, 55.38);
+
+#endif
