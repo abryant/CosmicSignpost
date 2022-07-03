32c3f02270f34cb536d3fdef369410c975194024
Anthony Bryant <anthony@bryants.eu>
2022-07-03 12:45:06 +0100

    Add TimeMillisMicros

--- lib/spacepointer/time_utils.cc
+++ lib/spacepointer/time_utils.cc
@@ -1,8 +1,61 @@
 #include "time_utils.h"
 
 #include <cstdint>
+#include <cstddef>
+
+#include <sys/time.h>
 
 double daysSinceJ2000(int64_t unixTimeMillis) {
   int64_t timeSinceJ2000Millis = unixTimeMillis - J2000_UTC_MILLIS;
   return timeSinceJ2000Millis / 1000.0 / 60.0 / 60.0 / 24.0;
+}
+
+TimeMillisMicros::TimeMillisMicros()
+    : millis(0), micros(0) {}
+
+TimeMillisMicros::TimeMillisMicros(int64_t millis, int32_t micros)
+    : millis(millis), micros(micros) {}
+
+TimeMillisMicros TimeMillisMicros::now() {
+  struct timeval time_now;
+  while (true) {
+    int result = gettimeofday(&time_now, NULL);
+    if (result == 0) {
+      break;
+    }
+  }
+  int64_t resultMillis = (((int64_t) time_now.tv_sec) * 1000) + (((int64_t) time_now.tv_usec) / 1000);
+  int32_t resultMicros = time_now.tv_usec % 1000;
+  return TimeMillisMicros(resultMillis, resultMicros);
+}
+
+TimeMillisMicros TimeMillisMicros::ofMicros(int32_t micros) {
+  return TimeMillisMicros(0, micros);
+}
+
+TimeMillisMicros TimeMillisMicros::plusMicros(int32_t plusMicros) const {
+  int32_t newMicros = micros + plusMicros;
+  int32_t extraMillis = newMicros / 1000;
+  return TimeMillisMicros(millis + extraMillis, newMicros % 1000);
+}
+
+int32_t positiveMod1000(int32_t micros) {
+  int32_t result = micros % 1000;
+  if (result < 0) {
+    result += 1000;
+  }
+  return result;
+}
+
+TimeMillisMicros TimeMillisMicros::minusMicros(int32_t subtractedMicros) const {
+  int32_t newMicros = micros - subtractedMicros;
+  int32_t newMod1000 = positiveMod1000(newMicros);
+  int64_t extraMillis = (newMicros - newMod1000) / 1000;
+  return TimeMillisMicros(millis + extraMillis, newMod1000);
+}
+
+int64_t TimeMillisMicros::deltaMicrosSince(TimeMillisMicros other) const {
+  int64_t millisDiff = millis - other.millis;
+  int32_t microsDiff = micros - other.micros;
+  return millisDiff * 1000 + microsDiff;
 }
--- lib/spacepointer/time_utils.h
+++ lib/spacepointer/time_utils.h
@@ -8,4 +8,40 @@ const int64_t J2000_UTC_MILLIS = 946727935816;
 
 double daysSinceJ2000(int64_t unixTimeMillis);
 
+class TimeMillisMicros {
+  public:
+    // always positive
+    int64_t millis;
+    // always positive, and in the range [0,1000)
+    int32_t micros;
+
+    TimeMillisMicros();
+    TimeMillisMicros(int64_t millis, int32_t micros);
+    static TimeMillisMicros now();
+    static TimeMillisMicros ofMicros(int32_t micros);
+    TimeMillisMicros plusMicros(int32_t addedMicros) const;
+    TimeMillisMicros minusMicros(int32_t subtractedMicros) const;
+    int64_t deltaMicrosSince(TimeMillisMicros other) const;
+
+    // operator<=> only works in C++20, and ESP32 can't use that yet.
+    [[nodiscard]] constexpr bool operator==(const TimeMillisMicros& other) const {
+      return millis == other.millis && micros == other.micros;
+    }
+    [[nodiscard]] constexpr bool operator!=(const TimeMillisMicros& other) const {
+      return !(*this == other);
+    }
+    [[nodiscard]] constexpr bool operator<(const TimeMillisMicros& other) const {
+      return millis < other.millis || (millis == other.millis && micros < other.micros);
+    }
+    [[nodiscard]] constexpr bool operator>(const TimeMillisMicros& other) const {
+      return other < *this;
+    }
+    [[nodiscard]] constexpr bool operator<=(const TimeMillisMicros& other) const {
+      return *this < other || *this == other;
+    }
+    [[nodiscard]] constexpr bool operator>=(const TimeMillisMicros& other) const {
+      return other <= *this;
+    }
+};
+
 #endif
