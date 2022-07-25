8285438abd7ebd7e895162d2bcf738932772afd5
Anthony Bryant <anthony@bryants.eu>
2022-07-25 21:11:48 +0100

    Add menu handling to the arduino main file

--- src/main_arduino.cc
+++ src/main_arduino.cc
@@ -1,31 +1,53 @@
 #ifndef UNIT_TEST
 
+#include <memory>
 #include <Arduino.h>
 
 #include <AccelStepper.h>
 #include <WiFiManager.h>
+#include <ESP32Encoder.h>
+
+#include "input_devices.h"
+#include "menu.h"
+#include "space_pointer_menu.h"
 
 #include "ota.h"
 #include "secrets.h"
 
-const int32_t STEP_PIN = 2;
-const int32_t DIR_PIN = 4;
+const int32_t AZIMUTH_STEP_PIN = 2;
+const int32_t AZIMUTH_DIR_PIN = 4;
+const int32_t ALTITUDE_DIR_PIN = 19;
+const int32_t ALTITUDE_STEP_PIN = 18;
+
+const int32_t I2C_SDA_PIN = 21;
+const int32_t I2C_SCL_PIN = 22;
+
+const int32_t BUTTON_PIN = 27;
+
+const int32_t ENCODER_A_PIN = 12;
+const int32_t ENCODER_B_PIN = 14;
+const int32_t ENCODER_BUTTON_PIN = 13;
+
 const int32_t STEPS_PER_ROTATION = 200*16;
 
 WiFiManager wifiManager;
-AccelStepper stepper = AccelStepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
-
-int64_t lastTime;
+AccelStepper stepper = AccelStepper(AccelStepper::DRIVER, AZIMUTH_STEP_PIN, AZIMUTH_DIR_PIN);
+std::shared_ptr<Menu> menu;
 
 void setup() {
   Serial.begin(115200);
+
+  menu = buildSpacePointerMenu();
+  InputDevices::initRotaryEncoder(ENCODER_A_PIN, ENCODER_B_PIN);
+  InputDevices::initSelectButton(ENCODER_BUTTON_PIN);
+  InputDevices::initBackButton(BUTTON_PIN);
+
   wifiManager.setHostname("spacepointer");
   wifiManager.autoConnect("Space Pointer", WIFI_MANAGER_PASSWORD.c_str());
   ota::setUp(1420, OTA_PASSWORD);
 
   stepper.setMaxSpeed(5000.0);
   stepper.setAcceleration(1000.0);
-  lastTime = millis();
 }
 
 void loop() {
@@ -33,6 +55,7 @@ void loop() {
     stepper.moveTo(stepper.currentPosition() + (STEPS_PER_ROTATION * 5));
     delay(1000);
   }
+  InputDevices::controlMenu(*menu);
   stepper.run();
   ota::checkForOta();
 }
