116e7048314a481b5921479d16f47453476875d2
Anthony Bryant <anthony@bryants.eu>
2022-07-30 20:22:25 +0100

    Add a menu option for showing the IP address

--- lib/menu/space_pointer_menu.cc
+++ lib/menu/space_pointer_menu.cc
@@ -4,6 +4,7 @@
 #include <string>
 
 #include "Arduino.h"
+#include "WiFi.h"
 
 #include "menu.h"
 #include "menu_entry.h"
@@ -14,10 +15,20 @@
 
 std::shared_ptr<Menu> buildSpacePointerMenu() {
   std::vector<std::shared_ptr<MenuEntry>> mainEntries = {
-    std::make_shared<ActionMenuEntry>("Action", []() {}),
-    std::make_shared<InfoMenuEntry>("Info", []() {return "Some info";}),
-    std::make_shared<BooleanMenuEntry>("Boolean", [](bool b) {}, false),
-    std::make_shared<NumberMenuEntry>("Latitude", "Lt: ~###.######N", [](std::string value) {})
+    std::make_shared<InfoMenuEntry>("Wireless IP", []() {
+      return std::string(WiFi.localIP().toString().c_str());
+    }),
+    std::make_shared<ActionMenuEntry>("Action", []() {
+      Serial.println("Action pressed");
+    }),
+    std::make_shared<BooleanMenuEntry>("Boolean", [](bool b) {
+      Serial.print("Boolean changed to: ");
+      Serial.println(b);
+    }, false),
+    std::make_shared<NumberMenuEntry>("Latitude", "Lt: ~##.######N", [](std::string value) {
+      Serial.print("Latitude: ");
+      Serial.println(value.c_str());
+    })
   };
   return std::make_shared<Menu>("Main menu", mainEntries);
 }
