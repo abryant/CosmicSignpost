394e0a0a8882d7eb462753b0308e554bda2f43e0
Anthony Bryant <anthony@bryants.eu>
2022-08-01 23:32:39 +0100

    Make the main Arduino program control the motors

--- src/main_arduino.cc
+++ src/main_arduino.cc
@@ -6,20 +6,24 @@
 
 #include <AccelStepper.h>
 #include <WiFiManager.h>
-#include <ESP32Encoder.h>
+#include "freertos/task.h"
 
 #include "input_devices.h"
 #include "output_devices.h"
 #include "menu.h"
 #include "space_pointer_menu.h"
 
+#include "direction_queue.h"
+#include "stepper_motors.h"
+#include "time_utils.h"
+
 #include "ota.h"
 #include "secrets.h"
 
 const int32_t AZIMUTH_STEP_PIN = 2;
 const int32_t AZIMUTH_DIR_PIN = 4;
-const int32_t ALTITUDE_DIR_PIN = 19;
 const int32_t ALTITUDE_STEP_PIN = 18;
+const int32_t ALTITUDE_DIR_PIN = 19;
 
 const int32_t STEPS_PER_ROTATION = 200*16;
 
@@ -38,11 +42,24 @@ WiFiManager wifiManager;
 AccelStepper stepper = AccelStepper(AccelStepper::DRIVER, AZIMUTH_STEP_PIN, AZIMUTH_DIR_PIN);
 std::shared_ptr<Menu> menu;
 
+TaskHandle_t motorControlTaskHandle;
+std::shared_ptr<DirectionQueue> directionQueue;
+TimeMillisMicros lastAddedTime;
+
+std::shared_ptr<StepperMotors> motors;
+
+void controlStepperMotors(void *param) {
+  (void) param;
+
+  while (true) {
+    motors->control();
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
 
-  menu = buildSpacePointerMenu();
   InputDevices::initRotaryEncoder(ENCODER_A_PIN, ENCODER_B_PIN);
   InputDevices::initSelectButton(ENCODER_BUTTON_PIN);
   InputDevices::initBackButton(BUTTON_PIN);
@@ -54,6 +71,26 @@ void setup() {
 
   stepper.setMaxSpeed(5000.0);
   stepper.setAcceleration(1000.0);
+  directionQueue = std::make_shared<DirectionQueue>();
+
+  motors = std::make_shared<StepperMotors>(
+      directionQueue,
+      AZIMUTH_STEP_PIN,
+      AZIMUTH_DIR_PIN,
+      ALTITUDE_STEP_PIN,
+      ALTITUDE_DIR_PIN);
+
+  xTaskCreatePinnedToCore(
+      &controlStepperMotors,
+      "ControlMotors",
+      /* stackSize= */ 10000,
+      /* parameter= */ NULL,
+      /* priority= */ 0,
+      &motorControlTaskHandle,
+      /* core= */ 0);
+
+  menu = buildSpacePointerMenu();
+  lastAddedTime = TimeMillisMicros::now();
 }
 
 void loop() {
@@ -61,6 +98,13 @@ void loop() {
   std::string menuText = menu->getDisplayedText();
   // OutputDevices won't send it to the LCD again unless it has changed.
   OutputDevices::display(menuText);
+
+  lastAddedTime = lastAddedTime.plusMicros(5000);
+  Direction direction = Direction(90, 0);
+  if (!directionQueue->isFull()) {
+    int64_t timeMillis = (lastAddedTime.millis / 5) * 5;
+    directionQueue->addDirection(timeMillis, direction);
+  }
 
   ota::checkForOta();
 }
