7600f3623579cea093adcca94f045926eeb19691
Anthony Bryant <anthony@bryants.eu>
2022-09-20 00:04:13 +0100

    Use JPL Horizons orbital elements for the Moon
    These are inaccurate for any time other than J2000, but are a useful
    starting point for getting everything else working at J2000.

--- lib/spacepointer/moon_orbit.cc
+++ lib/spacepointer/moon_orbit.cc
@@ -6,36 +6,45 @@
 #include "angle_utils.h"
 #include "cartesian_location.h"
 #include "keplerian_orbit.h"
+#include "time_utils.h"
+
+double EARTH_MASS = 5.97219e24; // kg
+double MOON_MASS = 7.349e22; // kg
+double EARTH_DISTANCE_SCALE = MOON_MASS / (EARTH_MASS + MOON_MASS);
+double MOON_DISTANCE_SCALE = EARTH_MASS / (EARTH_MASS + MOON_MASS);
 
 CartesianLocation MoonOrbit::positionAt(int64_t timeMillis) {
-  double semiMajorAxisMetres = 384400000;
-  double eccentricity = 0.0554;
-  double inclinationRadians = degreesToRadians(5.16);
-  double longitudeOfAscendingNodeRadians = degreesToRadians(125.08);
-  double argumentOfPeriapsisRadians = degreesToRadians(318.15);
-  double meanAnomalyJ2000Radians = degreesToRadians(135.27);
-  double periodJulianDays = 27.322;
+  double timeJulianDaysSinceJ2000 = daysSinceJ2000(timeMillis);
+  double d = timeJulianDaysSinceJ2000;
+  // TODO: find better time-based values for all of these orbital elements, based on d
+  (void) d;
+  // Osculating Orbital Elements from JPL Horizons:
+  // https://ssd.jpl.nasa.gov/horizons/app.html
+  // Target Body: Moon [Luna]
+  // Coordinate Center: Geocentric [code: 500]
+  // Time Specification: Start=2000-01-01 TDB, Stop=2000-01-02, Step=6 (hours)
+  double semiMajorAxisMetres = 381'874'524.8499886;
+  double eccentricity = 0.06314721685094304;
+  double inclinationRadians = degreesToRadians(5.240273000309245);
+  double longitudeOfAscendingNodeRadians = degreesToRadians(123.9580554371928);
+  double argumentOfPeriapsisRadians = degreesToRadians(308.9226727206595);
+  double meanAnomalyRadians = degreesToRadians(146.6732747774804);
 
-  return KeplerianOrbit::findPosition(
-      timeMillis,
-      ReferenceFrame::EARTH_ECLIPTIC,
-      semiMajorAxisMetres,
-      eccentricity,
-      inclinationRadians,
-      longitudeOfAscendingNodeRadians,
-      argumentOfPeriapsisRadians,
-      meanAnomalyJ2000Radians,
-      periodJulianDays);
+  CartesianLocation moonFromEarth =
+      KeplerianOrbit::findPosition(
+          ReferenceFrame::EARTH_ECLIPTIC,
+          semiMajorAxisMetres,
+          eccentricity,
+          inclinationRadians,
+          longitudeOfAscendingNodeRadians,
+          argumentOfPeriapsisRadians,
+          meanAnomalyRadians);
+  return moonFromEarth;
 }
 
 CartesianLocation MoonOrbit::earthMoonBarycentreAt(int64_t timeMillis) {
-  double earthMass = 5.97217e24; // kg
-  double moonMass = 7.342e22; // kg
-  double scale = moonMass / (moonMass + earthMass);
   CartesianLocation moonLocationFromEarth = positionAt(timeMillis);
   return CartesianLocation(
-      moonLocationFromEarth.x * scale,
-      moonLocationFromEarth.y * scale,
-      moonLocationFromEarth.z * scale,
+      moonLocationFromEarth.position * EARTH_DISTANCE_SCALE,
       ReferenceFrame::EARTH_ECLIPTIC);
 }
