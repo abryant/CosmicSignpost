56a32c7d0fc24f269e3675c83dac56ccce4bd10f
Anthony Bryant <anthony@bryants.eu>
2022-09-20 21:00:35 +0100

    Add millisToJulianDays()

--- lib/spacepointer/time_utils.cc
+++ lib/spacepointer/time_utils.cc
@@ -6,8 +6,11 @@
 #include <sys/time.h>
 
 double daysSinceJ2000(int64_t unixTimeMillis) {
-  int64_t timeSinceJ2000Millis = unixTimeMillis - J2000_UTC_MILLIS;
-  return timeSinceJ2000Millis / 1000.0 / 60.0 / 60.0 / 24.0;
+  return millisToJulianDays(unixTimeMillis - J2000_UTC_MILLIS);
+}
+
+double millisToJulianDays(int64_t milliseconds) {
+  return milliseconds / 1000.0 / 60.0 / 60.0 / 24.0;
 }
 
 TimeMillisMicros::TimeMillisMicros()
