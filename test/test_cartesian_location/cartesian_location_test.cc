a11fe89176f748542373da7f3e958fd34d7a182a
Anthony Bryant <anthony@bryants.eu>
2022-09-22 23:56:08 +0100

    Add more cartesian location tests

--- test/test_cartesian_location/cartesian_location_test.cc
+++ test/test_cartesian_location/cartesian_location_test.cc
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 #include <cmath>
 
+#include "location.h"
 #include "time_utils.h"
 
 TEST(CartesianLocation, Towards) {
@@ -119,24 +120,53 @@ TEST(CartesianLocation, DirectionTowardsSouthEast_FromSouthernHemisphere) {
   EXPECT_DOUBLE_EQ(d.getAltitude(), 0);
 }
 
-TEST(CartesianLocation, EquatorialToFixedWhenRotationAngleIsZero) {
-  // At 05:17:17.331 after J2000, the Earth Rotation Angle is very close to zero.
-  int64_t timeMillis = J2000_UTC_MILLIS + 19037331;
+TEST(CartesianLocation, EquatorialToFixed_WhenRotationAngleIsZero) {
+  // The prime meridian and equinox are aligned 0.277537528 days before midnight on 1st January 2000.
+  int64_t timeMillis = approxUt1ToUnixTime(JANUARY_1_2000_MIDNIGHT_UTC_MILLIS - 0.277537528 * 24*60*60*1000);
   CartesianLocation fixed = CartesianLocation(Vector(1, 2, 3), ReferenceFrame::EARTH_EQUATORIAL).toFixed(timeMillis);
-  EXPECT_NEAR(fixed.position.getX(), 1, 0.000001);
-  EXPECT_NEAR(fixed.position.getY(), 2, 0.000001);
-  EXPECT_DOUBLE_EQ(fixed.position.getZ(), 3);
+  EXPECT_NEAR(fixed.position.getX(), 1, 0.001);
+  EXPECT_NEAR(fixed.position.getY(), 2, 0.001);
+  EXPECT_NEAR(fixed.position.getZ(), 3, 0.001);
   EXPECT_EQ(fixed.referenceFrame, ReferenceFrame::EARTH_FIXED);
 }
 
-TEST(CartesianLocation, EquatorialToFixedWhenRotationAngleIs90Degrees) {
-  // At 11:16:18.356 after J2000, the Earth Rotation Angle is very close to 90 degrees.
-  int64_t timeMillis = J2000_UTC_MILLIS + 40578356;
+TEST(CartesianLocation, EquatorialToFixed_WhenRotationAngleIs90Degrees) {
+  // The prime meridian and equinox are aligned 0.277537528 days before midnight on 1st January 2000.
+  // This test looks at a time 1/4 of a sidereal day (which is 86164 seconds of the normal 86400) after that.
+  int64_t timeMillis = approxUt1ToUnixTime(JANUARY_1_2000_MIDNIGHT_UTC_MILLIS + (-0.277537528 + (0.25*86164/86400)) * 24*60*60*1000);
   CartesianLocation fixed = CartesianLocation(Vector(1, 2, 3), ReferenceFrame::EARTH_EQUATORIAL).toFixed(timeMillis);
-  EXPECT_NEAR(fixed.position.getX(), -2, 0.000001);
-  EXPECT_NEAR(fixed.position.getY(), 1, 0.000001);
-  EXPECT_DOUBLE_EQ(fixed.position.getZ(), 3);
+  EXPECT_NEAR(fixed.position.getX(), 2, 0.01);
+  EXPECT_NEAR(fixed.position.getY(), -1, 0.01);
+  EXPECT_NEAR(fixed.position.getZ(), 3, 0.01);
   EXPECT_EQ(fixed.referenceFrame, ReferenceFrame::EARTH_FIXED);
+}
+
+TEST(CartesianLocation, EclipticToFixed) {
+  // The prime meridian and equinox are aligned 0.277537528 days before midnight on 1st January 2000.
+  int64_t timeMillis = approxUt1ToUnixTime(JANUARY_1_2000_MIDNIGHT_UTC_MILLIS - 0.277537528 * 24*60*60*1000);
+  CartesianLocation fixed = CartesianLocation(Vector(1, 2, 3), ReferenceFrame::EARTH_ECLIPTIC).toFixed(timeMillis);
+  EXPECT_NEAR(fixed.position.getX(), 1, 0.001);
+  EXPECT_NEAR(fixed.position.getY(), 0.64, 0.01);
+  EXPECT_NEAR(fixed.position.getZ(), 3.55, 0.01);
+  EXPECT_EQ(fixed.referenceFrame, ReferenceFrame::EARTH_FIXED);
+}
+
+TEST(CartesianLocation, EclipticToDirection_AtJ2000) {
+  // From JPL Horizons:
+  // https://ssd.jpl.nasa.gov/horizons/app.html
+  // Ephemeris Type: Vector Table
+  // Target Body: Moon [Luna]
+  // Coordinate Center: Geocentric [code: 500]
+  // Time Specification: Start=2000-01-01 TDB , Stop=2000-01-02, Step=6 (hours)
+  CartesianLocation moonEcliptic(
+      Vector(-2.916083841877129E+08, -2.749797416731504E+08, 3.627119662699287E+07),
+      ReferenceFrame::EARTH_ECLIPTIC);
+  Location zeroZero = Location(0, 0, 0);
+  Direction dir = zeroZero.getCartesian().directionTowards(moonEcliptic.toFixed(J2000_UTC_MILLIS), zeroZero.getNormal());
+  // TODO: figure out why JPL Horizons returns 257.206634 but we get 257.311769
+  EXPECT_NEAR(dir.getAzimuth(), 257.3 - 360.0, 0.1);
+  // TODO: figure out why JPL Horizons returns 30.568243 but we get 29.810948
+  EXPECT_NEAR(dir.getAltitude(), 30.57, 1);
 }
 
 #include "test_runner.inc"
